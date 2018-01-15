19c9580ad49a82ec1e52358c024854f3b52f72f9
Jessie Richardson <jessie.richardson04@gmail.com>
2018-01-14 21:10:22 -0500

    fixes indent styles

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -6,10 +6,12 @@ import GithubLogo from '../img/github_icon.png';
 class Search extends Component {
   constructor(props) {
     super(props)
-    this.state = { username: '',
-    			   clicked: false,
-                   sendData: false,
-                   showRepos: false}
+    this.state = {
+      username: '',
+    	clicked: false,
+      sendData: false,
+      showRepos: false
+    }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,47 +19,50 @@ class Search extends Component {
   }
 
   resetForm() {
-    this.setState({ username: '',
-    				sendData: false,
-                    showRepos: false})
+    this.setState({
+      username: '',
+    	sendData: false,
+      showRepos: false})
   }
 
   handleChange(event) {
     this.setState({
-        username: event.target.value
+      username: event.target.value
     })
   }
 
   handleSubmit(event) {
-    this.setState({ clicked: true,
-    				sendData: true,
-                    showRepos: false })
+    this.setState({
+      clicked: true,
+    	sendData: true,
+      showRepos: false })
     event.preventDefault();
   }
 
   renderUserInfo() {
-    return <Badge username={this.state.username}
-                  resetForm={this.resetForm}
-                  sendData={this.state.sendData}/>
+    return <Badge
+      username={this.state.username}
+      resetForm={this.resetForm}
+      sendData={this.state.sendData}/>
   }
 
   render() {
     return (
-        <div>
-            <header>
-                <img src={GithubLogo} style={{height: 50}} />
-                <h2>Find a Github User</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <input type="text"
-                                username={this.state.username}
-                                value={this.state.username}
-                                onChange={this.handleChange} />
-                    </label>
-                        <input type="submit" value="Submit" />
-                </form>
-            </header>
-            {this.state.clicked && this.renderUserInfo()}
+      <div>
+        <header>
+          <img src={GithubLogo} style={{height: 50}} />
+          <h2>Find a Github User</h2>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                <input type="text"
+                       username={this.state.username}
+                       value={this.state.username}
+                       onChange={this.handleChange} />
+              </label>
+                <input type="submit" value="Submit" />
+            </form>
+        </header>
+        {this.state.clicked && this.renderUserInfo()}
       </div>
     )
   }
